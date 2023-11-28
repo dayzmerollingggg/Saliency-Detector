2cf2d5de23a666092689810aeeb5b84ae1bf5d2d
Sowresh Mecheri-Senthil <138344579+SowreshMS@users.noreply.github.com>
2023-11-28 01:43:03 -0600

    Create saliency.py

---
+++ saliency.py
@@ -0,0 +1,22 @@
+import cv2
+import matplotlib.pyplot as plt
+from google.colab.patches import cv2_imshow
+
+# Load an image
+image_path = "/content/Trance.png"  # Replace with the actual path
+image = cv2.imread(image_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Initialize the Spectral Residual Saliency Detector
+saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
+
+# Compute the saliency map
+(success, saliencyMap) = saliency.computeSaliency(image)
+
+# Rescale the saliency map to the range [0, 255]
+saliencyMap = (saliencyMap * 255).astype("uint8")
+
+# Display the original image and the saliency map
+plt.imshow(image)
+plt.show()
+plt.imshow(saliencyMap)
